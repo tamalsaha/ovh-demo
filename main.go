67515869ead52dc69b629eaaac2cc61acb7cb212
tamal <tamal@appscode.com>
2017-11-25 18:08:46 -0800

    added-all

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/ovh/go-ovh/ovh"
+)
 
+// PartialMe holds the first name of the currently logged-in user.
+// Visit https://api.ovh.com/console/#/me#GET for the full definition
+type PartialMe struct {
+	Firstname string `json:"firstname"`
+}
+
+// Instantiate an OVH client and get the firstname of the currently logged-in user.
+// Visit https://api.ovh.com/createToken/index.cgi?GET=/me to get your credentials.
+func main() {
+	var me PartialMe
+
+	client, _ := ovh.NewClient(
+		"ovh-eu",
+		"YOUR_APPLICATION_KEY",
+		"YOUR_APPLICATION_SECRET",
+		"YOUR_CONSUMER_KEY",
+	)
+	client.Get("/me", &me)
+	fmt.Printf("Welcome %s!\n", me.Firstname)
 }
